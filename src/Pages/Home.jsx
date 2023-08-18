187be2f1250fc76d47e18f9758a89c59f5f5124b
LALedesma007 <laledesma@hotmail.com.ar>
2023-08-17 21:14:31 -0300

    Agragado las ofertas

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CarouselHome from '../Components/Carousel/CarouselHome'
-
+import Offers from '../Components/Offers/Offers'
 
 const Home = () => {
   return (
     <div>
       <CarouselHome/>
+      <Offers/>
     </div>
   )
 }
