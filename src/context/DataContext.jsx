1afce3c10959bab2bdd4bdf8b709724eaae8842f
LALedesma007 <laledesma@hotmail.com.ar>
2023-08-20 11:27:32 -0300

    Se modificó ofertas

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,51 @@
+import { createContext } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+export const dataContext = createContext();
+
+const DataProvider = ({children}) => {
+
+  const [data, setData] = useState([])
+  const [cartPhone, setCartPhone] = useState([])
+  const [cartheadset, setCartHeadset] = useState([])
+  const [cartgamer , SetCartGamer] = useState ([])
+  const [cartwatch , SetCartWatch] = useState ([])
+  const [cartrandom, Setcartrandom] = useState ([])
+  const [dataOffer, SetdataOffer] = useState ([])
+
+  const getallproduct = async() =>{
+    try {
+      const resp = await axios.get('https://smartphone-deploy.onrender.com/api/getproduct');  
+      setData(resp.data.getData)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getallproduct()
+  }, [])
+
+  const getallproductOffer = async() =>{
+    try {
+      const resp = await axios.get('https://smartphone-deploy.onrender.com/api/getproductoffer');  
+      SetdataOffer(resp.data.getDataOffers)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getallproductOffer()
+  }, [])
+  
+
+  return <dataContext.Provider value={{ data, cartPhone, setCartPhone, cartheadset, setCartHeadset, cartgamer , SetCartGamer, cartwatch , SetCartWatch, cartrandom, Setcartrandom, dataOffer}}>{children}</dataContext.Provider>
+ 
+}
+
+export default DataProvider
+
